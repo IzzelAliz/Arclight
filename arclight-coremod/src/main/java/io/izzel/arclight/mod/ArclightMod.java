5302b8f0089489508e07d09a2e7b9e5bc293b63d
IzzelAliz <csh2001331@126.com>
2020-05-19 10:19:11 +0800

    Do not show incompatible client.

--- arclight-coremod/src/main/java/io/izzel/arclight/mod/ArclightMod.java
+++ arclight-coremod/src/main/java/io/izzel/arclight/mod/ArclightMod.java
@@ -2,7 +2,11 @@ package io.izzel.arclight.mod;
 
 import io.izzel.arclight.mod.server.event.ArclightEventDispatcherRegistry;
 import net.minecraftforge.fml.CrashReportExtender;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bukkit.craftbukkit.v1_14_R1.CraftCrashReport;
@@ -16,5 +20,6 @@ public class ArclightMod {
         LOGGER.info("Arclight Mod loaded.");
         ArclightEventDispatcherRegistry.registerAllEventDispatchers();
         CrashReportExtender.registerCrashCallable("Arclight", () -> new CraftCrashReport().call().toString());
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
     }
 }
