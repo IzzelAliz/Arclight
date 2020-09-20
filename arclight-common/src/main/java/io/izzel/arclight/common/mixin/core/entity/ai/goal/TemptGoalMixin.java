1cdaf319ddb5e54f30b5f419f741d1c20b99d9d4
IzzelAliz <csh2001331@126.com>
2020-09-20 10:43:40 +0800

    Fix tempting is not working (#46)

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/entity/ai/goal/TemptGoalMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/entity/ai/goal/TemptGoalMixin.java
@@ -8,6 +8,7 @@ import org.bukkit.craftbukkit.v.entity.CraftHumanEntity;
 import org.bukkit.craftbukkit.v.event.CraftEventFactory;
 import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,7 +25,7 @@ public abstract class TemptGoalMixin {
     @Shadow @Final protected CreatureEntity creature;
     // @formatter:on
 
-    @Inject(method = "shouldExecute", cancellable = true, at = @At(value = "INVOKE_ASSIGN", ordinal = 0, target = "Lnet/minecraft/world/World;getClosestPlayer(Lnet/minecraft/entity/EntityPredicate;Lnet/minecraft/entity/LivingEntity;)Lnet/minecraft/entity/player/PlayerEntity;"))
+    @Inject(method = "shouldExecute", cancellable = true, at = @At(value = "FIELD", shift = At.Shift.AFTER, opcode = Opcodes.PUTFIELD, target = "Lnet/minecraft/entity/ai/goal/TemptGoal;closestPlayer:Lnet/minecraft/entity/player/PlayerEntity;"))
     public void arclight$tempt(CallbackInfoReturnable<Boolean> cir) {
         boolean tempt = this.closestPlayer != null && (this.isTempting(this.closestPlayer.getHeldItemMainhand()) || this.isTempting(this.closestPlayer.getHeldItemOffhand()));
         if (tempt) {
