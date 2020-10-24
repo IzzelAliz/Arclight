6c8d6d832a6684532f0c6856334963bbcd46cdd7
IzzelAliz <csh2001331@126.com>
2020-10-24 17:55:29 +0800

    Fix potion effect values may be null

---
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/PotionEffectTypeMixin.java
@@ -0,0 +1,28 @@
+package io.izzel.arclight.common.mixin.bukkit;
+
+import org.bukkit.potion.PotionEffectType;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Arrays;
+
+@Mixin(PotionEffectType.class)
+public class PotionEffectTypeMixin {
+
+    @Shadow @Final private static PotionEffectType[] byId;
+
+    /**
+     * @author IzzelAliz
+     * @reason
+     */
+    @Overwrite
+    @NotNull
+    public static PotionEffectType[] values() {
+        int from = byId[0] == null ? 1 : 0;
+        int to = byId[byId.length - 1] == null ? byId.length - 1 : byId.length;
+        return Arrays.copyOfRange(byId, from, to);
+    }
+}
