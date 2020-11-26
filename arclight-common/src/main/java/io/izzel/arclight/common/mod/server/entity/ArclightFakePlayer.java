96457372d78040e938d7483821ac8808991c4fe7
IzzelAliz <csh2001331@126.com>
2020-11-26 19:45:40 +0800

    Fake player fix (#80)

--- arclight-common/src/main/java/io/izzel/arclight/common/mod/server/entity/ArclightFakePlayer.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mod/server/entity/ArclightFakePlayer.java
@@ -1,5 +1,6 @@
 package io.izzel.arclight.common.mod.server.entity;
 
+import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import org.bukkit.craftbukkit.v.CraftServer;
 import org.bukkit.craftbukkit.v.entity.CraftPlayer;
@@ -12,7 +13,8 @@ public class ArclightFakePlayer extends CraftPlayer {
 
     @Override
     public boolean isOp() {
-        return this.getHandle().getGameProfile().getId() != null && super.isOp();
+        GameProfile profile = this.getHandle().getGameProfile();
+        return profile != null && profile.getId() != null && super.isOp();
     }
 
     @Override
