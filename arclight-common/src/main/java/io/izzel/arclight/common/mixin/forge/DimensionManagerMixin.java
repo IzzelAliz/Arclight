fffbf40183ab0d03e6445d5722c74554f7ffa6db
IzzelAliz <csh2001331@126.com>
2020-11-27 20:46:39 +0800

    Also call events when world loads

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/forge/DimensionManagerMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/forge/DimensionManagerMixin.java
@@ -7,6 +7,9 @@ import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.common.DimensionManager;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v.CraftWorld;
+import org.bukkit.event.world.WorldInitEvent;
+import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,7 +28,12 @@ public abstract class DimensionManagerMixin {
 
     @Inject(method = "initWorld", locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", target = "Lnet/minecraftforge/eventbus/api/IEventBus;post(Lnet/minecraftforge/eventbus/api/Event;)Z"))
     private static void arclight$updateWorldInit(MinecraftServer server, DimensionType dim, CallbackInfoReturnable<ServerWorld> cir, ServerWorld overworld, ServerWorld world) {
-        ((WorldBridge) world).bridge$getWorld();
+        CraftWorld craftWorld = ((WorldBridge) world).bridge$getWorld();
+        if (((WorldBridge) world).bridge$getGenerator() != null) {
+            craftWorld.getPopulators().addAll(((WorldBridge) world).bridge$getGenerator().getDefaultPopulators(craftWorld));
+        }
+        Bukkit.getPluginManager().callEvent(new WorldInitEvent(craftWorld));
+        Bukkit.getPluginManager().callEvent(new WorldLoadEvent(craftWorld));
     }
 
     @Redirect(method = "unloadWorlds", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/DimensionManager;canUnloadWorld(Lnet/minecraft/world/server/ServerWorld;)Z"))
