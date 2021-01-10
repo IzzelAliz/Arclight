f94adc9225565b7a7fd0bfe50de656a01991d8f5
IzzelAliz <csh2001331@126.com>
2021-01-10 17:14:38 +0800

    Fix class cast exception on mod villagers

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/CraftAbstractVillagerMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/CraftAbstractVillagerMixin.java
@@ -5,7 +5,7 @@ import org.bukkit.craftbukkit.v.entity.CraftAbstractVillager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-@Mixin(CraftAbstractVillager.class)
+@Mixin(value = CraftAbstractVillager.class, remap = false)
 public class CraftAbstractVillagerMixin extends CraftEntityMixin {
 
     /**
