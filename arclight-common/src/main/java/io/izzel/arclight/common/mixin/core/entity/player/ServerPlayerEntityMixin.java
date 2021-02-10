2f4e7e9a58eb1261a6054b316358bf369efc3558
IzzelAliz <csh2001331@126.com>
2021-02-10 23:16:00 +0800

    Fix opening mod containers

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/entity/player/ServerPlayerEntityMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/entity/player/ServerPlayerEntityMixin.java
@@ -667,7 +667,9 @@ public abstract class ServerPlayerEntityMixin extends PlayerEntityMixin implemen
         if (container != null) {
             ((ContainerBridge) container).bridge$setTitle(itileinventory.getDisplayName());
             boolean cancelled = false;
+            ArclightCaptures.captureContainerOwner((ServerPlayerEntity)(Object)this);
             container = CraftEventFactory.callInventoryOpenEvent((ServerPlayerEntity) (Object) this, container, cancelled);
+            ArclightCaptures.resetContainerOwner();
             if (container == null && !cancelled) {
                 if (itileinventory instanceof IInventory) {
                     ((IInventory) itileinventory).closeInventory((ServerPlayerEntity) (Object) this);
