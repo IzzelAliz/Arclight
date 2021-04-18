77378c43f193a8d1abcbb4f5c5f7872af4250a19
IzzelAliz <csh2001331@126.com>
2021-04-18 11:28:09 +0800

    Fix champions crash (#235)

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/util/IndirectEntityDamageSourceMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/util/IndirectEntityDamageSourceMixin.java
@@ -2,14 +2,21 @@ package io.izzel.arclight.common.mixin.core.util;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.util.IndirectEntityDamageSource;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import io.izzel.arclight.common.bridge.util.IndirectEntityDamageSourceBridge;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(IndirectEntityDamageSource.class)
 public class IndirectEntityDamageSourceMixin extends DamageSourceMixin implements IndirectEntityDamageSourceBridge {
 
+    // @formatter:off
+    @Shadow @Final private Entity indirectEntity;
+    // @formatter:on
+
     public Entity getProximateDamageSource() {
-        return super.getTrueSource();
+        Entity trueSource = super.getTrueSource();
+        return trueSource == null ? this.indirectEntity : trueSource;
     }
 
     @Override
