cbf9065ad49d137bfb28a0bf835790bc43b736e2
IzzelAliz <csh2001331@126.com>
2021-08-04 12:55:37 +0800

    Fix RemappingURLClassLoader zip file closed (#336)

--- arclight-common/src/main/java/io/izzel/arclight/common/mod/util/remapper/generated/RemappingURLClassLoader.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mod/util/remapper/generated/RemappingURLClassLoader.java
@@ -16,6 +16,7 @@ import java.net.URLConnection;
 import java.net.URLStreamHandlerFactory;
 import java.security.CodeSource;
 import java.util.concurrent.Callable;
+import java.util.jar.Manifest;
 
 public class RemappingURLClassLoader extends URLClassLoader implements RemappingClassLoader {
 
@@ -41,40 +42,42 @@ public class RemappingURLClassLoader extends URLClassLoader implements Remapping
         String path = name.replace('.', '/').concat(".class");
         URL resource = this.getResource(path);
         if (resource != null) {
+            URLConnection connection;
+            Callable<byte[]> byteSource;
+            Manifest manifest;
             try {
-                URLConnection connection;
-                Callable<byte[]> byteSource;
-                try {
-                    connection = resource.openConnection();
-                    connection.connect();
-                    byteSource = () -> {
-                        try (InputStream is = connection.getInputStream()) {
-                            byte[] classBytes = ByteStreams.toByteArray(is);
-                            classBytes = SwitchTableFixer.INSTANCE.processClass(classBytes);
-                            return classBytes;
-                        }
-                    };
-                } catch (IOException e) {
-                    throw new ClassNotFoundException(name, e);
+                connection = resource.openConnection();
+                connection.connect();
+                if (connection instanceof JarURLConnection && ((JarURLConnection) connection).getManifest() != null) {
+                    manifest = ((JarURLConnection) connection).getManifest();
+                } else {
+                    manifest = null;
                 }
-
-                Product2<byte[], CodeSource> classBytes = this.getRemapper().remapClass(name, byteSource, connection);
-
-                int i = name.lastIndexOf('.');
-                if (i != -1) {
-                    String pkgName = name.substring(0, i);
-                    if (getPackage(pkgName) == null) {
-                        if (connection instanceof JarURLConnection && ((JarURLConnection) connection).getManifest() != null) {
-                            this.definePackage(pkgName, ((JarURLConnection) connection).getManifest(), ((JarURLConnection) connection).getJarFileURL());
-                        } else {
-                            this.definePackage(pkgName, null, null, null, null, null, null, null);
-                        }
+                byteSource = () -> {
+                    try (InputStream is = connection.getInputStream()) {
+                        byte[] classBytes = ByteStreams.toByteArray(is);
+                        classBytes = SwitchTableFixer.INSTANCE.processClass(classBytes);
+                        return classBytes;
                     }
-                }
-                result = this.defineClass(name, classBytes._1, 0, classBytes._1.length, classBytes._2);
+                };
             } catch (IOException e) {
                 throw new ClassNotFoundException(name, e);
             }
+
+            Product2<byte[], CodeSource> classBytes = this.getRemapper().remapClass(name, byteSource, connection);
+
+            int i = name.lastIndexOf('.');
+            if (i != -1) {
+                String pkgName = name.substring(0, i);
+                if (getPackage(pkgName) == null) {
+                    if (manifest != null) {
+                        this.definePackage(pkgName, manifest, ((JarURLConnection) connection).getJarFileURL());
+                    } else {
+                        this.definePackage(pkgName, null, null, null, null, null, null, null);
+                    }
+                }
+            }
+            result = this.defineClass(name, classBytes._1, 0, classBytes._1.length, classBytes._2);
         }
         if (result == null) {
             throw new ClassNotFoundException(name);
