9fd4ecc8b0cd3f09f2492b05da8c0b5042aa3752
IzzelAliz <csh2001331@126.com>
2021-11-09 12:45:59 +0800

    Update to forge 36.2.9

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/raid/RaidManagerMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/raid/RaidManagerMixin.java
@@ -3,6 +3,7 @@ package io.izzel.arclight.common.mixin.core.world.raid;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.raid.Raid;
 import net.minecraft.world.raid.RaidManager;
@@ -15,6 +16,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.List;
 import java.util.Map;
 
 @Mixin(RaidManager.class)
@@ -26,7 +28,7 @@ public class RaidManagerMixin {
 
     @Inject(method = "badOmenTick", cancellable = true, locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/raid/Raid;increaseLevel(Lnet/minecraft/entity/player/PlayerEntity;)V"))
     public void arclight$raidTrigger(ServerPlayerEntity playerEntity, CallbackInfoReturnable<Raid> cir,
-                                     DimensionType dimensionType, BlockPos pos, BlockPos pos1, Raid raid) {
+                                     DimensionType dimensionType, BlockPos pos, BlockPos pos1, List<?> list, int i, Vector3d vec, Raid raid) {
         if (!CraftEventFactory.callRaidTriggerEvent(raid, playerEntity)) {
             playerEntity.removePotionEffect(Effects.BAD_OMEN);
             this.byId.remove(raid.getId(), raid);
