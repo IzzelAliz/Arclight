7ed1b5d2457eec9f0f3b2a07dff5588c6c4e39d9
IzzelAliz <csh2001331@126.com>
2021-12-03 21:27:49 +0800

    Fix bungee compatibility

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/network/protocol/handshake/CHandshakePacketMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/network/protocol/handshake/CHandshakePacketMixin.java
@@ -8,8 +8,11 @@ import net.minecraftforge.network.NetworkConstants;
 import net.minecraftforge.network.NetworkHooks;
 import org.spigotmc.SpigotConfig;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Objects;
 
@@ -18,6 +21,8 @@ public class CHandshakePacketMixin {
 
     private static final String EXTRA_DATA = "extraData";
     private static final Gson GSON = new Gson();
+
+    @Shadow public String hostName;
 
     @Redirect(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/FriendlyByteBuf;readUtf(I)Ljava/lang/String;"))
     private String arclight$bungeeHostname(FriendlyByteBuf packetBuffer, int maxLength) {
@@ -34,11 +39,22 @@ public class CHandshakePacketMixin {
                 for (Property property : properties) {
                     if (Objects.equals(property.getName(), EXTRA_DATA)) {
                         String extraData = property.getValue().replace("\1", "\0");
+                        this.arclight$host = ip;
                         return NetworkHooks.getFMLVersion(split[0] + extraData);
                     }
                 }
             }
         }
         return fmlVersion;
+    }
+
+    private transient String arclight$host;
+
+    @Inject(method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V", at = @At("RETURN"))
+    private void arclight$writeBack(FriendlyByteBuf p_179801_, CallbackInfo ci) {
+        if (arclight$host != null) {
+            this.hostName = arclight$host;
+            arclight$host = null;
+        }
     }
 }
