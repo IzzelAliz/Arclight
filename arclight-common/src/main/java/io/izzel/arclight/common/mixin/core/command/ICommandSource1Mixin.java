b6d6df7f748f3cdae7e473287f734d60b36da97a
sandtechnology <20417547+sandtechnology@users.noreply.github.com>
2022-03-07 17:23:29 +0800

    [1.16] Fix AbstractMethodError when using DUMMY command source (#496)
    * Fix AbstractMethodError when using DUMMY command source
    
    * Bump to 1.0.23-SNAPSHOT

---
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/command/ICommandSource1Mixin.java
@@ -0,0 +1,49 @@
+package io.izzel.arclight.common.mixin.core.command;
+
+import io.izzel.arclight.common.bridge.command.ICommandSourceBridge;
+import net.minecraft.command.CommandSource;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v.command.ServerCommandSender;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(targets = "net/minecraft/command/ICommandSource$1")
+public class ICommandSource1Mixin implements ICommandSourceBridge {
+
+
+    public CommandSender getBukkitSender(CommandSource wrapper) {
+        return new ServerCommandSender() {
+            private final boolean isOp = wrapper.hasPermissionLevel(wrapper.getServer().getOpPermissionLevel());
+
+            @Override
+            public boolean isOp() {
+                return isOp;
+            }
+
+            @Override
+            public void setOp(boolean value) {
+            }
+
+            @Override
+            public void sendMessage(@NotNull String message) {
+
+            }
+
+            @Override
+            public void sendMessage(@NotNull String[] messages) {
+
+            }
+
+            @NotNull
+            @Override
+            public String getName() {
+                return "DUMMY";
+            }
+        };
+    }
+
+    @Override
+    public CommandSender bridge$getBukkitSender(CommandSource wrapper) {
+        return getBukkitSender(wrapper);
+    }
+}
