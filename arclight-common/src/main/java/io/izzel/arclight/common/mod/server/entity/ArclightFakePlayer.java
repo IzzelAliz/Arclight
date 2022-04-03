024185951c2044ed12926bdec2090accaf822dd3
IzzelAliz <csh2001331@126.com>
2022-04-03 21:24:21 +0800

    Fix null game mode crash

--- arclight-common/src/main/java/io/izzel/arclight/common/mod/server/entity/ArclightFakePlayer.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mod/server/entity/ArclightFakePlayer.java
@@ -2,8 +2,10 @@ package io.izzel.arclight.common.mod.server.entity;
 
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import org.bukkit.GameMode;
 import org.bukkit.craftbukkit.v.CraftServer;
 import org.bukkit.craftbukkit.v.entity.CraftPlayer;
+import org.jetbrains.annotations.NotNull;
 
 public class ArclightFakePlayer extends CraftPlayer {
 
@@ -19,5 +21,11 @@ public class ArclightFakePlayer extends CraftPlayer {
 
     @Override
     public void setOp(boolean value) {
+    }
+
+    @Override
+    public @NotNull GameMode getGameMode() {
+        GameMode gameMode = super.getGameMode();
+        return gameMode == null ? GameMode.SURVIVAL : gameMode;
     }
 }
