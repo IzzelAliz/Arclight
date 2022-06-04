e20bc556664e35fb6bfe2b9f975f51f9dc56dba2
Daniel Norris <dnorris443@gmail.com>
2022-06-04 21:44:54 +0100

    perf(entity): redirect Entity#getPassengers to the Arclight brdige

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/entity/EntityMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/entity/EntityMixin.java
@@ -594,6 +594,11 @@ public abstract class EntityMixin implements InternalEntityBridge, EntityBridge,
         }
     }
 
+    @Inject(method = "getPassengers", cancellable = true, at = @At(value = "HEAD"))
+    public void arclight$getPassengers(CallbackInfoReturnable<List<Entity>> cir) {
+        cir.setReturnValue(this.bridge$getPassengers());
+    }
+
     @Override
     public List<Entity> bridge$getPassengers() {
         return passengers;
