156b78b4de1f59e820fd67c593afac702f75eb0e
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2022-06-07 16:13:57 +0100

    Optimize Entity#getPassengers (#604)
    * perf(entity): redirect Entity#getPassengers to the Arclight brdige
    
    * perf(entity): add getPassengers mixins to stop copying the List
    
    * fix(entity): revert old broken change and update import for Opcodes
    
    * ref(optimizations): move to correct package and correct mixins config (forgot to push last one)
    
    * ref(bridge): move the shadow to correct position

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/entity/EntityMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/entity/EntityMixin.java
@@ -84,11 +84,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import javax.annotation.Nullable;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 @SuppressWarnings("ConstantConditions")
 @Mixin(Entity.class)
@@ -195,6 +191,7 @@ public abstract class EntityMixin implements InternalEntityBridge, EntityBridge,
     @Shadow protected boolean firstUpdate;
     @Shadow public abstract boolean isSilent();
     @Shadow public abstract void setInvisible(boolean invisible);
+    @Shadow protected abstract void getRecursivePassengers(boolean playersOnly, Set<Entity> passengers);
     // @formatter:on
 
     private static final int CURRENT_LEVEL = 2;
@@ -597,6 +594,11 @@ public abstract class EntityMixin implements InternalEntityBridge, EntityBridge,
     @Override
     public List<Entity> bridge$getPassengers() {
         return passengers;
+    }
+
+    @Override
+    public void bridge$getRecursivePassengers(boolean playersOnly, Set<Entity> passengers) {
+        this.getRecursivePassengers(playersOnly, passengers);
     }
 
     @Override
