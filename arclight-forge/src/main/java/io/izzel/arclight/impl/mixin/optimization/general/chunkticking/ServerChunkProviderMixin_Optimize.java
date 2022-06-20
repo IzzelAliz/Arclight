8edf4b5aaac19ad80b9efa1f776c0cdd6820dc10
Daniel Norris <dnorris443@gmail.com>
2022-06-20 14:02:55 +0100

    feat(chunks): remove unnecessary shuffling and allocation on chunk ticking

---
+++ arclight-forge/src/main/java/io/izzel/arclight/impl/mixin/optimization/general/chunkticking/ServerChunkProviderMixin_Optimize.java
@@ -0,0 +1,42 @@
+package io.izzel.arclight.impl.mixin.optimization.general.chunkticking;
+
+import io.izzel.arclight.common.bridge.world.server.ChunkManagerBridge;
+import jdk.internal.org.objectweb.asm.Opcodes;
+import net.minecraft.world.server.ChunkManager;
+import net.minecraft.world.server.ServerChunkProvider;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+@Mixin(ServerChunkProvider.class)
+public class ServerChunkProviderMixin_Optimize {
+
+    // @formatter:off
+    @Shadow @Final public ChunkManager chunkManager;
+    // @formatter:on
+
+    private static final ArrayList<?> EMPTY = new ArrayList<>(0);
+
+    @Redirect(method = "Lnet/minecraft/world/server/ServerChunkProvider;tickChunks()V",
+    at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayList(Ljava/lang/Iterable;)Ljava/util/ArrayList;"))
+    public ArrayList<?> handle(Iterable<?> elements) {
+        return EMPTY;
+    }
+
+    @Redirect(method = "Lnet/minecraft/world/server/ServerChunkProvider;tickChunks()V",
+            at = @At(value = "INVOKE", target = "Ljava/util/Collections;shuffle(Ljava/util/List;)V"))
+    public void handle(List<?> objects) {}
+
+    @Redirect(method = "Lnet/minecraft/world/server/ServerChunkProvider;tickChunks()V",
+    at = @At(value = "INVOKE", target = "Ljava/util/List;forEach(Ljava/util/function/Consumer;)V"))
+    public void forEach(List instance, Consumer consumer) {
+        ((ChunkManagerBridge) this.chunkManager).bridge$getLoadedChunksIterable().forEach(consumer);
+    }
+}
