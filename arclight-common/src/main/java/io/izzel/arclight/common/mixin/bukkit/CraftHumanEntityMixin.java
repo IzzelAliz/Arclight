759a3fd53e7ef11191c2fa722a902ef3f673e506
IzzelAliz <csh2001331@126.com>
2022-06-24 10:31:50 +0800

    Fix ender chest get lost across death (#623)

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/CraftHumanEntityMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/CraftHumanEntityMixin.java
@@ -5,11 +5,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import org.bukkit.craftbukkit.v.CraftServer;
 import org.bukkit.craftbukkit.v.entity.CraftEntity;
 import org.bukkit.craftbukkit.v.entity.CraftHumanEntity;
-import org.bukkit.craftbukkit.v.inventory.CraftInventory;
 import org.bukkit.craftbukkit.v.inventory.CraftInventoryPlayer;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = CraftHumanEntity.class, remap = false)
@@ -17,7 +14,6 @@ public abstract class CraftHumanEntityMixin extends CraftEntity {
 
     // @formatter:off
     @Shadow private CraftInventoryPlayer inventory;
-    @Shadow @Final @Mutable private CraftInventory enderChest;
     // @formatter:on
 
     public CraftHumanEntityMixin(CraftServer server, Entity entity) {
@@ -28,6 +24,5 @@ public abstract class CraftHumanEntityMixin extends CraftEntity {
     public void setHandle(Entity entity) {
         super.setHandle(entity);
         this.inventory = new CraftInventoryPlayer(((PlayerEntity) entity).inventory);
-        this.enderChest = new CraftInventory(((PlayerEntity) entity).getInventoryEnderChest());
     }
 }
