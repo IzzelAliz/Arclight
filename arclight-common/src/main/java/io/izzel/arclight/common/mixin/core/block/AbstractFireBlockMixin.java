8cb9df2cfe493a208f86b7c87e9fa0b020962d57
IzzelAliz <csh2001331@126.com>
2022-09-04 16:26:00 +0800

    Fix portal not light in plugin worlds (#677)

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/block/AbstractFireBlockMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/block/AbstractFireBlockMixin.java
@@ -1,13 +1,18 @@
 package io.izzel.arclight.common.mixin.core.block;
 
+import io.izzel.arclight.common.bridge.world.WorldBridge;
 import net.minecraft.block.AbstractFireBlock;
 import net.minecraft.block.Blocks;
+import net.minecraft.util.RegistryKey;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import org.bukkit.craftbukkit.v.event.CraftEventFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractFireBlock.class)
 public class AbstractFireBlockMixin {
@@ -18,5 +23,13 @@ public class AbstractFireBlockMixin {
             world.removeBlock(pos, isMoving);
         }
         return false;
+    }
+
+    @Inject(method = "canLightPortal", cancellable = true, at = @At("RETURN"))
+    private static void arclight$lightPortal(World world, CallbackInfoReturnable<Boolean> cir) {
+        if (!cir.getReturnValueZ()) {
+            RegistryKey<DimensionType> typeKey = ((WorldBridge) world).bridge$getTypeKey();
+            cir.setReturnValue(typeKey == DimensionType.OVERWORLD || typeKey == DimensionType.THE_NETHER);
+        }
     }
 }
