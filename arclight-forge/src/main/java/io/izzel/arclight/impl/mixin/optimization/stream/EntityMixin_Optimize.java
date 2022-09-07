8cfe9a288c9904e96c8e4521d3d5daed6d401b61
IzzelAliz <csh2001331@126.com>
2022-09-07 14:24:38 +0800

    Fix shadow field type mismatch (#622)
    Introduced in #604

--- arclight-forge/src/main/java/io/izzel/arclight/impl/mixin/optimization/stream/EntityMixin_Optimize.java
+++ arclight-forge/src/main/java/io/izzel/arclight/impl/mixin/optimization/stream/EntityMixin_Optimize.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -19,7 +19,7 @@ import java.util.stream.Stream;
 public abstract class EntityMixin_Optimize {
 
     // @formatter:off
-    @Shadow @Final public ImmutableList<Entity> passengers;
+    @Shadow @Final public List<Entity> passengers;
     // @formatter:on
 
     /**
