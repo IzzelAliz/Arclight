ce647b4caee3687599a64a4d5a8465b786b6f61a
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2022-09-18 03:12:58 +0100

    perf(entity): add patch for passenger optimizations (#735)
    * Revert "Revert "Optimize Entity#getPassengers (#604)" (#727)"
    
    This reverts commit 0231df94d878b0b5c23a829951efc9706a82198f.
    
    * fix(optimize): return self, not empty, in getSelfAndPassengers

---
+++ arclight-forge/src/main/java/io/izzel/arclight/impl/mixin/optimization/stream/AbstractHorseEntityMixin_Optimize.java
@@ -0,0 +1,26 @@
+package io.izzel.arclight.impl.mixin.optimization.stream;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.passive.horse.AbstractHorseEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(AbstractHorseEntity.class)
+public abstract class AbstractHorseEntityMixin_Optimize extends AnimalEntity {
+
+    protected AbstractHorseEntityMixin_Optimize(EntityType<? extends AnimalEntity> type, World worldIn) {
+        super(type, worldIn);
+    }
+
+    /**
+     * @author danorris709
+     * @reason `getPassengers` instantiates a new instance of the list when called
+     */
+    @Overwrite
+    public Entity getControllingPassenger() {
+        return this.passengers.isEmpty() ? null : this.passengers.get(0);
+    }
+}
