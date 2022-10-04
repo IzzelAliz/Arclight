e24aa7a275f4767f012827dae9cd7008f8a3851d
IzzelAliz <csh2001331@126.com>
2022-10-04 11:34:11 +0800

    Fix blood magic command crash (#633)

--- arclight-common/src/main/java/io/izzel/arclight/common/bridge/command/ICommandSourceBridge.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/bridge/command/ICommandSourceBridge.java
@@ -1,9 +1,12 @@
 package io.izzel.arclight.common.bridge.command;
 
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.ICommandSource;
 import org.bukkit.command.CommandSender;
 
 public interface ICommandSourceBridge {
 
-    CommandSender bridge$getBukkitSender(CommandSource wrapper);
+    default CommandSender bridge$getBukkitSender(CommandSource wrapper) {
+        return ((ICommandSourceBridge) ICommandSource.DUMMY).bridge$getBukkitSender(wrapper);
+    }
 }
