15f03f23656f2492eb46bd7adffc4295bd4c1ec1
IzzelAliz <csh2001331@126.com>
2022-12-08 21:21:09 +0800

    Fix mixin conflict with immersive weathering (#812)

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/level/block/RootedDirtBlockMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/level/block/RootedDirtBlockMixin.java
@@ -9,10 +9,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(RootedDirtBlock.class)
+// https://github.com/IzzelAliz/Arclight/issues/812
+@Mixin(value = RootedDirtBlock.class, priority = 1500)
 public class RootedDirtBlockMixin {
 
-    @Redirect(method = "performBonemeal", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlockAndUpdate(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
+    @Redirect(method = "performBonemeal", require = 0, at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;setBlockAndUpdate(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"))
     private boolean arclight$blockSpread(ServerLevel instance, BlockPos pos, BlockState state) {
         return CraftEventFactory.handleBlockSpreadEvent(instance, pos.above(), pos, state);
     }
