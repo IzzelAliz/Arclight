dc71c2c4096a03edd1ec47d452a5740aa6694452
IzzelAliz <csh2001331@126.com>
2023-05-19 10:44:40 +0800

    Fix recipe has empty result (#837)

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/item/crafting/ShapelessRecipeMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/item/crafting/ShapelessRecipeMixin.java
@@ -1,6 +1,7 @@
 package io.izzel.arclight.common.mixin.core.world.item.crafting;
 
 import io.izzel.arclight.common.bridge.core.item.crafting.IRecipeBridge;
+import io.izzel.arclight.common.mod.util.ArclightSpecialRecipe;
 import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -20,12 +21,15 @@ public class ShapelessRecipeMixin implements IRecipeBridge {
     @Shadow @Final ItemStack result;
     @Shadow @Final String group;
     @Shadow @Final NonNullList<Ingredient> ingredients;
-    // @formatter:off
+    // @formatter:on
 
     @Override
     public Recipe bridge$toBukkitRecipe() {
+        if (this.result.isEmpty()) {
+            return new ArclightSpecialRecipe((ShapelessRecipe) (Object) this);
+        }
         CraftItemStack result = CraftItemStack.asCraftMirror(this.result);
-        CraftShapelessRecipe recipe = new CraftShapelessRecipe(result, (ShapelessRecipe)(Object) this);
+        CraftShapelessRecipe recipe = new CraftShapelessRecipe(result, (ShapelessRecipe) (Object) this);
         recipe.setGroup(this.group);
         for (Ingredient list : this.ingredients) {
             recipe.addIngredient(CraftRecipe.toBukkit(list));
