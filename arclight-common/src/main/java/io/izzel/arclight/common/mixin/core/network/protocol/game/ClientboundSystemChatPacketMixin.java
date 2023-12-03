21cf262b5d41397df29dafb201d1e0ed3080c8f6
IzzelAliz <csh2001331@126.com>
2023-12-03 13:43:00 +0800

    Fix ProtocolLib index oob on chat packets (#1101 #1129)

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/network/protocol/game/ClientboundSystemChatPacketMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/network/protocol/game/ClientboundSystemChatPacketMixin.java
@@ -5,6 +5,9 @@ import net.md_5.bungee.chat.ComponentSerializer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundSystemChatPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientboundSystemChatPacket.class)
 public class ClientboundSystemChatPacketMixin {
@@ -19,5 +22,16 @@ public class ClientboundSystemChatPacketMixin {
 
     public void arclight$constructor(BaseComponent[] content, boolean overlay) {
         arclight$constructor(ComponentSerializer.toString(content), overlay);
+    }
+
+    @Inject(method = "<init>(Lnet/minecraft/network/chat/Component;Z)V", at = @At("RETURN"))
+    private void arclight$init(Component content, boolean overlay, CallbackInfo ci) {
+        this.content = Component.Serializer.toJson(content);
+    }
+
+    private String content;
+
+    public String content() {
+        return content;
     }
 }
