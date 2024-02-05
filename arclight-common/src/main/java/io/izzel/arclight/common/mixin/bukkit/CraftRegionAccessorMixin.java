1d6e6463bb71c04314e5f725bda1b249cd1239fc
rafucuk <raufsen11@gmail.com>
2024-02-06 02:52:15 +0300

    debug test

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/CraftRegionAccessorMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/bukkit/CraftRegionAccessorMixin.java
@@ -25,6 +25,8 @@ public abstract class CraftRegionAccessorMixin {
     @Inject(method = "spawnEntity(Lorg/bukkit/Location;Lorg/bukkit/entity/EntityType;)Lorg/bukkit/entity/Entity;", cancellable = true, at = @At("HEAD"))
     private void arclight$useFactory(Location loc, EntityType entityType, CallbackInfoReturnable<Entity> cir) {
         Function<Location, ? extends net.minecraft.world.entity.Entity> factory = ((EntityTypeBridge) (Object) entityType).bridge$entityFactory();
+        logger.log("RaufLog:" + entityType)
+
         if (factory != null) {
             cir.setReturnValue(this.addEntity(factory.apply(loc), CreatureSpawnEvent.SpawnReason.CUSTOM, null, true));
         }
@@ -33,6 +35,7 @@ public abstract class CraftRegionAccessorMixin {
     @Inject(method = "spawnEntity(Lorg/bukkit/Location;Lorg/bukkit/entity/EntityType;Z)Lorg/bukkit/entity/Entity;", cancellable = true, at = @At("HEAD"))
     private void arclight$useFactory(Location loc, EntityType entityType, boolean randomizeData, CallbackInfoReturnable<Entity> cir) {
         Function<Location, ? extends net.minecraft.world.entity.Entity> factory = ((EntityTypeBridge) (Object) entityType).bridge$entityFactory();
+        logger.log("RaufLog:" + entityType)
         if (factory != null) {
             cir.setReturnValue(this.addEntity(factory.apply(loc), CreatureSpawnEvent.SpawnReason.CUSTOM, null, randomizeData));
         }
