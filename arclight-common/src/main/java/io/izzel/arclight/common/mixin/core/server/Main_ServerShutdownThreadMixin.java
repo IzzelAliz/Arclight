0f2ca4bf184277d7b16a6f4fcee8f1967bce09ae
IzzelAliz <csh2001331@126.com>
2024-03-10 13:16:37 +0800

    Fix exit on main thread deadlock (#1256)

---
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/server/Main_ServerShutdownThreadMixin.java
@@ -0,0 +1,17 @@
+package io.izzel.arclight.common.mixin.core.server;
+
+import net.minecraft.server.MinecraftServer;
+import org.spigotmc.AsyncCatcher;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(targets = "net/minecraft/server/Main$1")
+public class Main_ServerShutdownThreadMixin {
+
+    @Redirect(method = "run", at = @At(value = "INVOKE", remap = true, target = "Lnet/minecraft/server/MinecraftServer;halt(Z)V"))
+    private void arclight$shutdown(MinecraftServer instance, boolean b) {
+        AsyncCatcher.enabled = false;
+        instance.close();
+    }
+}
