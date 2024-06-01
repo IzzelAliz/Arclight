ca30ff4b36e8bc37d4fb7e86a5b17cc38e09136b
IzzelAliz <csh2001331@126.com>
2024-06-01 11:41:25 +0800

    Bump api version, fix Java 22 compatibility

--- bootstrap/src/applaunch/java/io/izzel/arclight/server/Launcher.java
+++ bootstrap/src/applaunch/java/io/izzel/arclight/server/Launcher.java
@@ -11,6 +11,9 @@ public class Launcher {
     private static final int MIN_CLASS_VERSION = 61;
     private static final int MIN_JAVA_VERSION = 17;
 
+    private static final int MAX_CLASS_VERSION = 66;
+    private static final int MAX_JAVA_VERSION = 22;
+
     public static void main(String[] args) throws Throwable {
         int javaVersion = (int) Float.parseFloat(System.getProperty("java.class.version"));
         if (javaVersion < MIN_CLASS_VERSION) {
@@ -18,6 +21,13 @@ public class Launcher {
             System.err.println("Current: " + System.getProperty("java.version"));
             System.exit(-1);
             return;
+        }
+
+        if (javaVersion > MAX_CLASS_VERSION) {
+            System.err.println("Warning: Arclight is known to be compatible with up to Java " + MAX_JAVA_VERSION + " and may not run on later versions");
+            System.err.println("Current: " + System.getProperty("java.version"));
+            System.err.flush();
+            Thread.sleep(3000);
         }
 
         try (InputStream input = Launcher.class.getResourceAsStream("/META-INF/MANIFEST.MF")) {
