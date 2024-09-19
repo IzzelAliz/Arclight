9f8f2dac7406d93fe8e79e2bb72663fa85cb7194
秋雨落 <i@rain.cx>
2024-09-19 22:06:15 +0800

    Fix clicking slot smith table. (Closes #1491)
    Signed-off-by: 秋雨落 <i@rain.cx>

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/inventory/SmithingTableContainerMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/inventory/SmithingTableContainerMixin.java
@@ -2,6 +2,7 @@ package io.izzel.arclight.common.mixin.core.world.inventory;
 
 import io.izzel.arclight.common.bridge.core.entity.player.PlayerEntityBridge;
 import io.izzel.arclight.common.bridge.core.util.IWorldPosCallableBridge;
+import io.izzel.arclight.mixin.Decorate;
 import io.izzel.arclight.mixin.DecorationOps;
 import net.minecraft.world.inventory.ResultContainer;
 import net.minecraft.world.inventory.SmithingMenu;
@@ -14,14 +15,13 @@ import org.bukkit.craftbukkit.v.inventory.CraftItemStack;
 import org.bukkit.event.inventory.PrepareSmithingEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SmithingMenu.class)
 public abstract class SmithingTableContainerMixin extends ItemCombinerMixin {
 
     private CraftInventoryView bukkitEntity;
 
-    @Redirect(method = "createResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/ResultContainer;setItem(ILnet/minecraft/world/item/ItemStack;)V"))
+    @Decorate(method = "createResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/ResultContainer;setItem(ILnet/minecraft/world/item/ItemStack;)V"))
     private void arclight$prepareSmithing(ResultContainer craftResultInventory, int index, ItemStack stack) throws Throwable {
         PrepareSmithingEvent event = new PrepareSmithingEvent(getBukkitView(), CraftItemStack.asCraftMirror(stack).clone());
         Bukkit.getServer().getPluginManager().callEvent(event);
