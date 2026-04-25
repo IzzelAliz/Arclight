839ae24f9cbbfb2e7259115756cbd8413a2a4f56
LabsZero <LabsZero@ampznetwork.com>
2026-04-25 11:21:43 +0100

    Update ActualMerchantMenuMixin.java

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/inventory/ActualMerchantMenuMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/inventory/ActualMerchantMenuMixin.java
@@ -1,21 +1,20 @@
 package io.izzel.arclight.common.mixin.core.world.inventory;
 
 import io.izzel.arclight.common.bridge.core.world.IInventoryBridge;
+import net.minecraft.world.inventory.MerchantContainer;
+import net.minecraft.world.inventory.MerchantMenu;
 import org.bukkit.inventory.InventoryView;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 // // This Targets the actual Menu class to solve the NoSuchMethodError crash
-@Mixin(targets = "net.minecraft.world.inventory.MerchantMenu")
+@Mixin(MerchantMenu.class)
 public abstract class ActualMerchantMenuMixin {
 
-    // // Shadow the inventory inside the menu
-    @Shadow @Final private Object merchantInventory;
+    @Shadow @Final private MerchantContainer merchantInventory;
 
-    // // Inject the method the crash report is looking for
     public InventoryView getBukkitView() {
-        // // Ask the container we fixed in the other file for its Bukkit view
         return ((IInventoryBridge) this.merchantInventory).getBukkitView();
     }
 }
