f698dbddeb23151039610b857ec0bc4d98640e02
LabsZero <LabsZero@ampznetwork.com>
2026-04-25 09:36:00 +0100

    Fix null reference in getBukkitView method

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/inventory/MerchantMenuMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/inventory/MerchantMenuMixin.java
@@ -41,7 +41,7 @@ public abstract class MerchantMenuMixin implements IInventoryBridge, Container {
     @Override
     public InventoryView getBukkitView() {
         if (arclight$bukkitView == null) {
-            arclight$bukkitView = new CraftMerchantView(this.bridge$getBukkitInventory(), (net.minecraft.world.inventory.MerchantMenu) (Object) this);
+            arclight$bukkitView = new CraftMerchantView(this.bridge$getBukkitInventory(), (net.minecraft.world.inventory.MerchantMenu) null);
         }
         return arclight$bukkitView;
     }
