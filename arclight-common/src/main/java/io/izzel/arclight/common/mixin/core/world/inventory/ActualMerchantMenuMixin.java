8cb157d337c857de77ce5de88a7590b0f02e60e6
LabsZero <LabsZero@ampznetwork.com>
2026-04-25 12:19:22 +0100

    Update ActualMerchantMenuMixin.java

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/inventory/ActualMerchantMenuMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/world/inventory/ActualMerchantMenuMixin.java
@@ -3,21 +3,26 @@ package io.izzel.arclight.common.mixin.core.world.inventory;
 import io.izzel.arclight.common.bridge.core.world.IInventoryBridge;
 import net.minecraft.world.inventory.MerchantContainer;
 import net.minecraft.world.inventory.MerchantMenu;
+import org.bukkit.craftbukkit.v.inventory.view.CraftMerchantView;
 import org.bukkit.inventory.InventoryView;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(MerchantMenu.class)
 public abstract class ActualMerchantMenuMixin {
 
-    @Shadow(remap = false) 
-    @Final 
-    private MerchantContainer tradeContainer;
+    @Shadow(remap = false) @Final private MerchantContainer tradeContainer;
 
-    // // This satisfies the Spigot API requirement that MerchantMenu has getBukkitView()
+    @Unique
+    private InventoryView arclight$bukkitView;
+
     public InventoryView getBukkitView() {
-        // // Access the container via our bridge to get the Bukkit handle
-        return ((IInventoryBridge) this.tradeContainer).getBukkitView();
+        if (arclight$bukkitView == null) {
+            // // Create the view here, linking the internal container to the Spigot Menu
+            arclight$bukkitView = new CraftMerchantView(((IInventoryBridge) this.tradeContainer).bridge$getBukkitInventory(), (MerchantMenu) (Object) this);
+        }
+        return arclight$bukkitView;
     }
 }
